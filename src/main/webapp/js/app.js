684b9094abc29108d302848978e6cf82d428cadd
Johannes Simon <johannes.simon@mail.de>
2014-09-25 17:43:17 +0200

    - add error-handling of JSON request in app.js

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -16,35 +16,43 @@ var main = function() {
     });
 };
 
+var showAnswers = function(data) {
+    $.get('template.txt', function(answerTemplate) {
+	template = jsontemplate.Template(answerTemplate);
+	html = template.expand(data);
+	$('#results').html(html);
+	l.stop();
+	Prism.highlightAll();
+
+	$('#details').hide();
+	$('#dropdown-details').click(function() {
+	    $input = $( this );
+	    $target = $('#details');
+	    $target.slideToggle();
+	});
+
+	svg = Viz(data.dependency_tree, "svg");
+	$('#dependency_tree').html(svg);
+	width = parseInt($('svg').attr("width"));
+	height = parseInt($('svg').attr("height"));
+	$('svg').attr("width", width*0.8);
+	$('svg').attr("height", height*0.8);
+	$('text').attr("font-family", "arial");
+    });
+};
+
 var query = function(queryStr) {
     var sparqlEndpoint = $("input:radio[name='p']:checked").val();
     var queryUrl = 'ask?p=' + encodeURIComponent(sparqlEndpoint) + '&q=' + encodeURIComponent(queryStr);
     //var queryUrl = '/ask.json';
     $('#results').html("");
-    $.getJSON(queryUrl, function(data) {
-	$.get('template.txt', function(answerTemplate) {
-	    template = jsontemplate.Template(answerTemplate);
-	    html = template.expand(data);
-	    $('#results').html(html);
-	    l.stop();
-	    Prism.highlightAll();
-
-	    $('#details').hide();
-	    $('#dropdown-details').click(function() {
-		$input = $( this );
-		$target = $('#details');
-		$target.slideToggle();
-	    });
-	    
-	    svg = Viz(data.dependency_tree, "svg");
-	    $('#dependency_tree').html(svg);
-	    width = parseInt($('svg').attr("width"));
-	    height = parseInt($('svg').attr("height"));
-	    $('svg').attr("width", width*0.8);
-	    $('svg').attr("height", height*0.8);
-	    $('text').attr("font-family", "arial");
-	});
-    });
+    try {
+        $.getJSON(queryUrl, showAnswers)
+        	.fail(function(jqXHR, textStatus, errorThrown) { l.stop(); alert("Error retrieving JSON data (" + queryUrl + "): " + errorThrown); });
+    } catch (err) {
+	l.stop();
+	alert("Error retrieving JSON data (URL " + queryUrl + "): " + err);
+    }
 };
 
 
